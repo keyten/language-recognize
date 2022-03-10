9ef39c9fcb64198c0b57cbfcf0186a0c8764ad2e
Dmitriy <dmitriy@canva.com>
2022-03-10 17:41:07 +1100

    Update en.ts

--- langs/en.ts
+++ langs/en.ts
@@ -1,7 +1,13 @@
 export default [{
   n: 2, // 2 char combinations
-  [{
+  grams: [{
     val: 'th',
     num: 330 // per 10000 chars
+  }]
+ }, {
+  n: 3,
+  grams: [{
+    val: 'the',
+    num: 3
   }]
  }];
