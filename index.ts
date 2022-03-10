204259c708a9540ab44008c8fb354e7e820327d9
Dmitriy <dmitriy@canva.com>
2022-03-10 17:39:55 +1100

    Create index.ts

---
+++ index.ts
@@ -0,0 +1,14 @@
+interface NCharsTable {}
+
+function getNCharsFromText(text: string, n: number) {
+  // ...
+}
+
+function distanceBetweenTables() {}
+
+export default function (text: string, tables: NCharsTable[]) {
+  // for each table in tables:
+  // get distance between getNCharsFromText(text) and table
+  // find the closest one
+  // return
+}
