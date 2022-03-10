b2921ade312db9b3bd83e573c77c2f904c57572d
Dmitriy <ikeyten@gmail.com>
2022-03-10 18:05:26 +1100

    Update en.ts

--- langs/en.ts
+++ langs/en.ts
@@ -11,3 +11,38 @@ export default [{
     num: 3
   }]
  }];
+
+/*
+More advanced method:
+
+Order Of Frequency Of Single Letters
+E T A O I N S H R D L U
+
+Order Of Frequency Of Digraphs
+th er on an re he in ed nd ha at en es of or nt ea ti to it st io le is ou ar as de rt ve
+
+Order Of Frequency Of Trigraphs
+the and tha ent ion tio for nde has nce edt tis oft sth men
+
+Order Of Frequency Of Most Common Doubles
+ss ee tt ff ll mm oo
+
+Order Of Frequency Of Initial Letters
+T O A W B C D S F M R H I Y E G L N P U J K
+
+Order Of Frequency Of Final Letters
+E S T D N R Y F L O G H A K M P U W
+
+One-Letter Words
+a, I.
+
+Most Frequent Two-Letter Words
+of, to, in, it, is, be, as, at, so, we, he, by, or, on, do, if, me, my, up, an, go, no, us, am
+
+Most Frequent Three-Letter Words
+the, and, for, are, but, not, you, all, any, can, had, her, was, one, our, out, day, get, has, him, his, how, man, new, now, old, see, two, way, who, boy, did, its, let, put, say, she, too, use
+
+Most Frequent Four-Letter Words
+that, with, have, this, will, your, from, they, know, want, been, good, much, some, time
+
+*/
